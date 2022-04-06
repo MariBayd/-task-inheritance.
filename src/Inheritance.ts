31879e645791a791bd6a2c431b9cf0f3ca48b728
MariBayd <masha.baydina@gmail.com>
2022-04-06 20:40:15 +0300

    Задание по наследованию ts

--- src/Inheritance.ts
+++ src/Inheritance.ts
@@ -16,6 +16,21 @@ export class Person {
     readonly _name: string;
     readonly _secondName: string;
     readonly _age: number;
+
+    constructor(person: IPersonData) {
+        this._name = person.name;
+        this._secondName = person.secondName;
+        this._age = person.age;
+    }
+    getData(): IPersonData {
+        let person = <IPersonData>{
+            name: this._name,
+            secondName: this._secondName,
+            age: this._age,
+        };
+
+        return person;
+    }
 }
 
 /*
@@ -24,4 +39,20 @@ export class Person {
  */
 export class Student extends Person {
     readonly _phone: string;
+
+    constructor(student: IStudentData) {
+        super(student);
+        this._phone = student.phone;
+    }
+
+    getData(): IStudentData {
+        let student = <IStudentData>{
+            name: this._name,
+            secondName: this._secondName,
+            age: this._age,
+            phone: this._phone,
+        };
+
+        return student;
+    }
 }
